d616b385786d3626a109cb770b316f6fd923a327
Take327 <gatewayseed2@gmail.com>
2022-07-20 21:41:39 +0900

    sassのコンパイル機能追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 
 module.exports = {
   mode: process.env.NODE_ENV || "development",
@@ -8,7 +10,8 @@ module.exports = {
   entry: {
     "dist/background": path.join(__dirname, "src/background/background.ts"),
     "dist/contents": path.join(__dirname, "src/contents/contents.ts"),
-    "dist/action/js/index":path.join(__dirname, "src/action/action.ts"),
+    "dist/action/js/index": path.join(__dirname, "src/action/action.ts"),
+    "dist/action/css/style":path.join(__dirname, "src/action/scss/style.scss"),
   },
   output: {
     path: __dirname,
@@ -25,6 +28,14 @@ module.exports = {
       {
         test: /\.html$/,
         loader: "html-loader"
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
       }
     ],
   },
@@ -33,11 +44,15 @@ module.exports = {
   },
   // publicディレクトリに配置する静的リソースやmanifest.json等を移送する
   plugins: [
-    new CopyWebpackPlugin({ patterns: [{ from: "public", to: "dist" }] }),
+    new CopyWebpackPlugin({ patterns: [{ from: "public/manifest.json", to: "dist" }] }),
     new HtmlWebpackPlugin({
       filename: "dist/action/index.html",
-      template: "src/action/index.html",
-      chunks:["dist/action/js/index"]
+      template: "public/index.html",
+      chunks: ["dist/action/js/index"]
     }),
+    new MiniCssExtractPlugin({
+			filename: 'dist/action/css/style.css'
+		}),
+    new FixStyleOnlyEntriesPlugin(),
   ],
 };
