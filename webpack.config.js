24fa30c0855269c9ce867e178daeeb10d17053d6
Take327 <gatewayseed2@gmail.com>
2022-07-26 22:40:49 +0900

    コメント追加

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,10 @@ module.exports = {
         'action/js/index': path.join(__dirname, 'src/action/action.ts'),
         'action/css/style': path.join(__dirname, 'src/action/scss/style.scss'),
     },
+    //出力の設定
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].js',
+        path: path.join(__dirname, 'dist'), //出力左記のフォルダ名を設定する
+        filename: '[name].js', //jsファイルのファイル名を指定する [name]とするとentryのキーでjsファイルを作成する
     },
     module: {
         rules: [
@@ -41,15 +42,18 @@ module.exports = {
     },
     // publicディレクトリに配置する静的リソースやmanifest.json等を移送する
     plugins: [
+        //CopyWebpackPlugin ビルド時にコピーするプラグイン
         new CopyWebpackPlugin({ patterns: [{ from: 'public/manifest.json', to: '' }] }),
         new CopyWebpackPlugin({ patterns: [{ from: 'public/icons', to: 'icons' }] }),
+        //HTMLに<script> や <link>を追加し出力するプラグイン
         new HtmlWebpackPlugin({
-            filename: 'action/index.html',
-            template: 'public/index.html',
-            chunks: ['action/js/index', 'action/css/style'],
+            filename: 'action/index.html', //ビルド後のファイル名
+            template: 'public/index.html', //対象とするファイル名
+            chunks: ['action/js/index', 'action/css/style'], //読み込みたいentryのキー
         }),
+        //scssをcssにビルドするプラグイン
         new MiniCssExtractPlugin({
-            filename: '[name].css',
+            filename: '[name].css', //[name].cssとすることでentryのキーを使用してcssを作成する
         }),
         new FixStyleOnlyEntriesPlugin(),
     ],
